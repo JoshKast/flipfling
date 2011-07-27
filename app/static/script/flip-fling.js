7a8e5343a4ad2744c6f0584e0fea40e5e90ec4ac
JoshKast <Josh.Kastelein@gmail.com>
2011-07-26 22:10:32 -0400

    delete lingering js file

--- app/static/script/flip-fling.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  $(function() {
-    return $('input:button').bind('click', function() {
-      $('input[name="score[shots]"]').val(this.value);
-      return $('form').submit();
-    });
-  });
-}).call(this);
